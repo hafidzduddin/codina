cb91c663cec912b6e8fb09d388aace11772b84b8
hafidzduddin <hafidzduddin@gmail.com>
2013-09-08 12:14:31 +0700

    Revert "params: replace printk(KERN_<LVL>...) with pr_<lvl>(...)"
    This reverts commit bf5c71b7c2cae01bf82f717cf934230574a93279.

--- kernel/params.c
+++ kernel/params.c
@@ -189,9 +189,10 @@ int parse_args(const char *name,
 		args = next_arg(args, &param, &val);
 		irq_was_disabled = irqs_disabled();
 		ret = parse_one(param, val, params, num, unknown);
-		if (irq_was_disabled && !irqs_disabled())
-			pr_warn("%s: option '%s' enabled irq's!\n",
-					doing, param);
+		if (irq_was_disabled && !irqs_disabled()) {
+			printk(KERN_WARNING "parse_args(): option '%s' enabled "
+					"irq's!\n", param);
+		}
 		switch (ret) {
 		case -ENOENT:
 			printk(KERN_ERR "%s: Unknown parameter `%s'\n",
